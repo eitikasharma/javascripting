eb2fa9c5f1673016fb717e916121f2e234359d84
eitikasharma <44219225+eitikasharma@users.noreply.github.com>
2019-11-22 19:17:23 +0530

    completed

---
+++ function-arguments.js
@@ -0,0 +1,8 @@
+let a=53
+let b=61
+let c=67
+console.log(math(a,b,c))
+
+function math (a, b, c){
+  return a+b*c
+}
---
+++ functions.js
@@ -0,0 +1,6 @@
+let a='bananas'
+console.log(eat(a))
+
+function eat (food) {
+return food + ' tasted really good.'
+}
